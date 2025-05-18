f862d2c4dbb2439fdc3c913211368172b6c9b995
aishareissani <aishareissani95@gmail.com>
2025-05-19 03:08:56 +0700

    font

--- uas_pti/src/components/loading_screen.jsx
+++ uas_pti/src/components/loading_screen.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "../styles.css";
 
 function LoadingScreen() {
-  return <div className="loadScreen"></div>;
+  return (
+    <div className="loadScreen">
+      <h1 className="judul">UCUP MENJELAJAHI NUSANTARA</h1>
+      <input type="button" value="NEXT"></input>
+    </div>
+  );
 }
 
 export default LoadingScreen;
