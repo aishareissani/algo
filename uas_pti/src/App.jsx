8f89941aae681cb6e8a096dc08b4d846ccc1e11c
aishareissani <aishareissani95@gmail.com>
2025-05-18 19:33:13 +0700

    test react + vite
    test react + vite

---
+++ uas_pti/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <h1>Halo Dunia dari Vite + React! hai</h1>
+      <p>
+        Edit <code>src/App.jsx</code> dan simpan untuk live reload.
+      </p>
+      <p>Hi semua test test</p>
+    </div>
+  );
+}
