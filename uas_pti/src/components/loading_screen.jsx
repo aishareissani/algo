7569fd875bb69d062ac488a9e1460bcc20e4e73a
aishareissani <aishareissani95@gmail.com>
2025-05-25 02:11:35 +0700

    choose chara
    tp design masih jelek

--- uas_pti/src/components/loading_screen.jsx
+++ uas_pti/src/components/loading_screen.jsx
@@ -9,13 +9,16 @@ function LoadingScreen() {
   };
 
   return (
-    <div className="loadScreen">
-      <h1 className="judul">
-        <div>UCUP</div>
-        <div>MENJELAJAHI</div>
-        <div>NUSANTARA</div>
+    <div className="loading-screen bg-loading-screen fixed top-0 left-0 w-screen h-screen bg-no-repeat bg-center bg-cover z-1000 flex flex-col items-center justify-center">
+      <h1 className="judul text-white text-judul uppercase flex flex-col items-center justify-center leading-[1.5] tracking-widest2 text-center select-none animate-fadeIn">
+        <div className="fade-line animate-fadeInLine delay-[500ms]">UCUP</div>
+        <div className="fade-line animate-fadeInLine delay-[1000ms]">MENJELAJAHI</div>
+        <div className="fade-line animate-fadeInLine delay-[1500ms]">NUSANTARA</div>
       </h1>
-      <button className="nextButton" onClick={handleNextClick}>
+      <button
+        onClick={handleNextClick}
+        className="nextButton font-pressstart text-btn rounded-[20px] py-[15px] px-[40px] cursor-pointer border-none transition-transform transition-shadow duration-500 opacity-0 animate-fadeInButton delay-[2000ms] shadow-btn-default bg-btn-gradient hover:scale-110 hover:shadow-btn-hover"
+      >
         NEXT
       </button>
     </div>
